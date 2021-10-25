1f3ccde89021355667d2fddc7d59695411008649
Nicolas Reyes <nreyes.dev@gmail.com>
2021-10-25 00:26:29 -0300

    handler wip unstable

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from http.server import HTTPServer
 from src.handlers import WeatherHandler
+from src.weather_client import WeatherClient
+from src.logger import log, OK as LOG_OK
 
 def main():
-    PORT = 8080 # TODO env var
-    server = HTTPServer(('', PORT), WeatherHandler)
-    print("WeatherAPI running on port {}".format(PORT))
+    PORT = 8081 # TODO env var
+    weather_client = WeatherClient()
+    server = HTTPServer(('', PORT), WeatherHandler(weather_client))
+    log(LOG_OK, "WeatherAPI running on port {}".format(PORT))
     server.serve_forever()
-
 
 if __name__ == '__main__':
     main()
--- src/handlers.py
+++ src/handlers.py
@@ -1,15 +1,28 @@
 from http.server import BaseHTTPRequestHandler
 import json
+from .logger import log, OK as LOG_OK
+
 
 PATH = "/weather"
 
 class WeatherHandler(BaseHTTPRequestHandler):
+    #def __init__(self, weather_client):
+        #FIXME ??
+        #self.weather_client = weather_client
+
     def do_GET(self):
+        # FIXME path changes when it has query parameters so... (I should probably use another http lib)
         if self.path == PATH:
+            response = None
+            try:
+                log(LOG_OK, "handling request")
+                response = self.weather_client.get_weather()
+            except AttributeError:
+                self.send_response(500)
+                response = {
+                    "error": "We're having techincal problems, please try again later"
+                }
             self.send_response(200)
-            response = {
-                "hello": "world"
-            }
         else:
             self.send_response(404)
             response = {
